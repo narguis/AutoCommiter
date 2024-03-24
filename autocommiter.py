e8252fff77b9ddfb144128a2317499ecc2f7c157
Guilherme Narguis <narguis.guilherme@gmail.com>
2024-03-24 17:10:39 -0300

    commits_today() function finished

--- autocommiter.py
+++ autocommiter.py
@@ -4,19 +4,35 @@ import subprocess
 import requests
 from datetime import datetime
 
-username = 'narguis'
-today = datetime.now().strftime("%Y-%m-%d")
-
 # Path to the files that will be gradually uploaded
 files_to_upload = ""
+username = "narguis"
+token = ""
+today = datetime.now().strftime("%Y-%m-%d")
 
-def commited_today():
-    api_url = "https://api.github.com/search/commits?q=author:narguis+committer-date:2024-03-22"
-    response = requests.get(api_url).json()
+# If False, you will only commit the difference between your desired commits and your commits today
+# If True, the code will always commit your desired amount of commits
+always_commit = False
+desired_commits = 1
+
+def commits_today():
+    api_url = f"https://api.github.com/search/commits?q=author:{username}+committer-date:{today}"
+    
+    # Remove this line if you want the code to access only your public commits
+    headers = {"Authorization": f"token {token}"}
+    # Remove the headers paramater if you want the code to access only your public commits
+    response = requests.get(api_url, headers=headers).json()
+
     print(response)
-    if response['total_count'] == 0:
-        return False
-    else:
-        return True
+    return response['total_count']
 
-print(commited_today())
+def commit(amount):
+    pass
+
+if not always_commit:
+    remaining = desired_commits - commits_today()
+    if remaining > 0:
+        commit(remaining)
+
+else:
+    commit(desired_commits)
