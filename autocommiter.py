7c23f871838921fe13a2a42a57efa96bbe426af9
Guilherme Narguis <narguis.guilherme@gmail.com>
2024-03-24 22:50:13 -0300

    finding uncomitted files implemented

--- autocommiter.py
+++ autocommiter.py
@@ -3,9 +3,10 @@ import random
 import subprocess
 import requests
 from datetime import datetime
+from git import Repo
 
-# Path to the files that will be gradually uploaded
-files_to_upload = ""
+# Paths to the files that will be gradually uploaded
+path_commits = "../LeetCode"
 username = "narguis"
 token = ""
 today = datetime.now().strftime("%Y-%m-%d")
@@ -15,19 +16,39 @@ today = datetime.now().strftime("%Y-%m-%d")
 always_commit = False
 desired_commits = 1
 
+
+def commit(path, message = ''):
+    pass
+
+
+def find_files(path):
+    uncommited = []
+    repo = Repo(path)
+    files = os.listdir(path)
+    
+    for file in files:
+        if not(repo.git.status('--porcelain', file).startswith('D')):
+            uncommited.append(file)
+            
+    print(uncommited)
+    
+    # for root, directories, files in os.walk(root_directory):
+    # for file in files:
+    #     file_path = os.path.join(root, file)
+    #     if not(repo.git.status('--porcelain', file_path).startswith('D')):
+    #         uncommited.append(file_path)
+
+    # print(uncommited)
+
 def commits_today():
     api_url = f"https://api.github.com/search/commits?q=author:{username}+committer-date:{today}"
     
-    # Remove this line if you want the code to access only your public commits
+    # Remove this line if you don't want the code to access your private commits
     headers = {"Authorization": f"token {token}"}
-    # Remove the headers paramater if you want the code to access only your public commits
+    # Remove the headers paramater if you don't want the code to access your private commits
     response = requests.get(api_url, headers=headers).json()
-
-    print(response)
+    
     return response['total_count']
-
-def commit(amount):
-    pass
 
 if not always_commit:
     remaining = desired_commits - commits_today()
@@ -36,3 +57,6 @@ if not always_commit:
 
 else:
     commit(desired_commits)
+
+
+find_files(path_commits)
